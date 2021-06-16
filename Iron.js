f91fd4c03845a56e8f33d9ee31c4852e58df0e18
greensnake789 <84211693+greensnake789@users.noreply.github.com>
2021-06-15 18:18:12 -0700

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,26 @@
+class Iron{
+    constructor(x,y,width,height){
+        var options = {
+            restitution : 0.8,
+            friction:3,
+            density:30
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        World.add(world, this.body);
+        this.width = width;
+        this.height = height;
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        strokeWeight(4);
+        stroke("yellow");
+        fill("pink");
+        rect(0,0, this.width, this.height);
+        pop();
+
+    }
+}
